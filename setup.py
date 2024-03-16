c25b1fb4fe40a1f73ffb1c66753adc83db79f327
Anupam <anupam.chaitanya@gmail.com>
2024-03-16 13:41:12 +0530

    EDA & Problem Statement

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def get_requirements(file_path:str)->List[str]:
     return requirements
 
 setup(
-name='mlproject',
+name='mlprojects',
 version='0.0.1',
 author='Anupam',
 author_email='anupamchaitanya1988@gmail.com',
